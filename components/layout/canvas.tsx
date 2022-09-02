b00b6cb677b5eee43b94c2a1634684fba45ff696
Gitter499 <rafayel.amirkhanyan@gmail.com>
2022-09-02 13:42:48 +0000

    Adding canvas  and _app

---
+++ components/layout/canvas.tsx
@@ -0,0 +1,13 @@
+import { FC, useState } from "react";
+
+export interface IProps {};
+
+export const SuperCanvas: FC<IProps> = (props) => {
+   const [ error, setError ] = useState(null);
+   const [ loading, setLoading ] = useState(false);
+    return (
+        <div>
+            
+        </div>
+    );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import dynamic from 'next/dynamic'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const LCanvas = dynamic(() => import('../components/layout/canvas'), {
+  ssr: true,
+})
 
+function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    setState({ router })
+  }, [router])
+
+  return (
+    <>
+      <Header title={pageProps.title} />
+      <Dom>
+        <Component {...pageProps} />
+      </Dom>
+      {Component?.r3f && <LCanvas>{Component.r3f(pageProps)}</LCanvas>}
+    </>
+  )
 export default MyApp
